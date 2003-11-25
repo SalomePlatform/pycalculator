1faca7eba91ef8f6ecf07d31ee2ae7c34a71c98c
crouzet <crouzet>
2003-11-25 13:31:25 +0000

    premiere version

---
+++ src/PYCALCULATOR/PYCALCULATOR.py
@@ -0,0 +1,219 @@
+import PYCALCULATOR_ORB__POA
+import SALOME_ComponentPy
+
+import SALOME_MED
+from libMEDMEM_Swig import *
+from libMedCorba_Swig import *
+
+class PYCALCULATOR (PYCALCULATOR_ORB__POA.PYCALCULATOR_Gen, SALOME_ComponentPy.SALOME_ComponentPy_i ):
+
+    def __init__(self, orb, poa, contID, containerName, instanceName,
+                 interfaceName):
+        print "Begin of PYCALCULATOR::__init__"
+        notif = 0
+        SALOME_ComponentPy.SALOME_ComponentPy_i.__init__(self, orb, poa, 
+	       contID, containerName, instanceName, interfaceName, notif)
+	self._naming_service=SALOME_ComponentPy.SALOME_NamingServicePy_i(self._orb)
+        print "End of PYCALCULATOR::__init__" 
+
+    def Add(self, field1, field2):
+        print "Begin of PYCALCULATOR::Add"
+        print "pointeur on first argument : ",field1
+        print "            second         : ",field2
+
+        bool = 1
+        nbOfComp1 = field1.getNumberOfComponents()
+        name1 = field1.getName()
+        description1 = field1.getDescription()
+
+        nbOfComp2 = field2.getNumberOfComponents()
+        name2 = field2.getName()
+        description2 = field2.getDescription()
+
+        if (nbOfComp1 != nbOfComp2): bool = 0
+
+        compName = []
+        compUnit = []
+        for k in range(nbOfComp1):
+            kp1 = k+1
+            compName.append(field1.getComponentName(kp1))
+            compUnit.append(field1.getComponentUnit(kp1))
+            if (bool):
+                if ((compName[k] != field2.getComponentName(kp1)) or (compUnit[k] != field2.getComponentUnit(kp1))):
+                    bool = 0
+
+        support1 = field1.getSupport()
+        entity1 = support1.getEntity()
+        mesh1 = support1.getMesh()
+
+        support2 = field2.getSupport()
+        entity2 = support2.getEntity()
+        mesh2 = support2.getMesh()
+
+        if (support1.isOnAllElements()):
+            lengthValue1 = mesh1.getNumberOfElements(entity1,SALOME_MED.MED_ALL_ELEMENTS)
+            number1 = []
+            for k in range(lengthValue1):
+                number1.append(k)
+        else:
+            lengthValue1 = support1.getNumberOfElements(SALOME_MED.MED_ALL_ELEMENTS)
+            number1 = support1.getNumber(SALOME_MED.MED_ALL_ELEMENTS)
+
+        if (support2.isOnAllElements()):
+            lengthValue2 = mesh2.getNumberOfElements(entity2,SALOME_MED.MED_ALL_ELEMENTS)
+            number2 = []
+            for k in range(lengthValue2):
+                number2.append(k)
+        else:
+            lengthValue2 = support2.getNumberOfElements(SALOME_MED.MED_ALL_ELEMENTS)
+            number2 = support2.getNumber(SALOME_MED.MED_ALL_ELEMENTS)
+
+        # comparision of each support: due to the fact that they are CORBA
+        # pointers, the comparision will be done directly throught the numbers
+
+        if (bool):
+            if ((lengthValue1 != lengthValue2) or (entity1 != entity2)):
+                bool = 0
+
+        if (bool):
+            for k in range(lengthValue1):
+                if (number1[k] != number2[k]):
+                    bool = 0
+
+        value1 = field1.getValue(SALOME_MED.MED_FULL_INTERLACE)
+
+        value2 = []
+        if (bool):
+            value2 = field2.getValue(SALOME_MED.MED_FULL_INTERLACE)
+
+        valueOut = []
+        if (bool):
+            print "CALCULATORPY::Add : The fields have the same support, they then can be added"
+            for k in range(lengthValue1*nbOfComp1):
+                valueOut.append((value1[k]+value2[k]))
+        else:
+            print "CALCULATORPY::Add : WARNING !! For some reason the fields have not the same support"
+            for k in range(lengthValue1*nbOfComp1):
+                valueOut.append(value1[k])
+
+        print "CALCULATORPY::Add : Creation of the local field, nbOfComp = ",nbOfComp1, " length = ",lengthValue1
+
+        fieldOutLocal = createLocalFieldDouble(nbOfComp1,lengthValue1)
+        fieldOutLocal.setValue(MED_FULL_INTERLACE,valueOut)
+        fieldOutLocal.setName("-new_Add_Field-")
+        fieldOutLocal.setDescription(description1)
+
+        for k in range(nbOfComp1):
+            kp1 = k + 1
+            fieldOutLocal.setComponentName(kp1,compName[k])
+            fieldOutLocal.setMEDComponentUnit(kp1,compUnit[k])
+
+        print "CALCULATORPY::Add : Creation of the CORBA field"
+
+        fieldOutCorba = createCorbaFieldDouble(support1,fieldOutLocal)
+
+        print "End of CALCULATORPY::Add"
+        return fieldOutCorba
+
+    def Mul(self, field1, x1):
+        print "Begin of CALCULATORPY::Mul"
+        print "pointeur on first argument : ",field1
+        print "            second         : ",x1
+
+        nbOfComp = field1.getNumberOfComponents()
+        name = field1.getName()
+        description = field1.getDescription()
+        support = field1.getSupport()
+        print ".... get Names and Unit"
+        compName = []
+        compUnit = []
+        for k in range(nbOfComp):
+            kp1 = k+1
+            compName.append(field1.getComponentName(kp1))
+            compUnit.append(field1.getComponentUnit(kp1))
+
+
+        print ".... get mesh"
+        mesh = support.getMesh()
+
+        if (support.isOnAllElements()):
+            lengthValue = mesh.getNumberOfElements(support.getEntity(),SALOME_MED.MED_ALL_ELEMENTS)
+        else:
+            lengthValue = support.getNumberOfElements(SALOME_MED.MED_ALL_ELEMENTS)
+
+        value1 = field1.getValue(SALOME_MED.MED_FULL_INTERLACE)
+        valueOut = []
+        for k in range(lengthValue*nbOfComp):
+            valueOut.append(x1*value1[k])
+
+        print "CALCULATORPY::Mul : Creation of the local field, nbOfComp = ",nbOfComp, " length = ",lengthValue
+
+        fieldOutLocal = createLocalFieldDouble(nbOfComp,lengthValue)
+        fieldOutLocal.setValue(MED_FULL_INTERLACE,valueOut)
+        fieldOutLocal.setName("-new_Mul_Field-")
+        fieldOutLocal.setDescription(description)
+
+        for k in range(nbOfComp):
+            kp1 = k + 1
+            fieldOutLocal.setComponentName(kp1,compName[k])
+            fieldOutLocal.setMEDComponentUnit(kp1,compUnit[k])
+
+        print "CALCULATORPY::Mul : Creation of the CORBA field"
+
+        fieldOutCorba = createCorbaFieldDouble(support,fieldOutLocal)
+
+        print "End of CALCULATORPY::Mul"
+        return fieldOutCorba
+
+    def Constant(self, field1, x1):
+        print "Begin of CALCULATORPY::Constant"
+        print "pointeur on first argument : ",field1
+        print "            second         : ",x1
+
+        nbOfComp = field1.getNumberOfComponents()
+        name = field1.getName()
+        description = field1.getDescription()
+        support = field1.getSupport()
+        compName = []
+        compUnit = []
+        for k in range(nbOfComp):
+            kp1 = k+1
+            compName.append(field1.getComponentName(kp1))
+            compUnit.append(field1.getComponentUnit(kp1))
+
+        mesh = support.getMesh()
+
+        if (support.isOnAllElements()):
+            lengthValue = mesh.getNumberOfElements(support.getEntity(),SALOME_MED.MED_ALL_ELEMENTS)
+        else:
+            lengthValue = support.getNumberOfElements(SALOME_MED.MED_ALL_ELEMENTS)
+
+        valueOut = []
+        for k in range(lengthValue*nbOfComp):
+            valueOut.append(x1)
+
+        print "CALCULATORPY::Constant : Creation of the local field, nbOfComp = ",nbOfComp, " length = ",lengthValue
+
+        fieldOutLocal = createLocalFieldDouble(nbOfComp,lengthValue)
+        fieldOutLocal.setValue(MED_FULL_INTERLACE,valueOut)
+        fieldOutLocal.setName("-new_Const_Field-")
+        fieldOutLocal.setDescription(description)
+
+        for k in range(nbOfComp):
+            kp1 = k + 1
+            fieldOutLocal.setComponentName(kp1,compName[k])
+            fieldOutLocal.setMEDComponentUnit(kp1,compUnit[k])
+
+        print "CALCULATORPY::Constant : Creation of the CORBA field"
+
+        fieldOutCorba = createCorbaFieldDouble(support,fieldOutLocal)
+
+        print "End of CALCULATORPY::Constant"
+        return fieldOutCorba
+
+    def writeMEDfile(self, field1, fileName):
+        print "Begin of CALCULATORPY::writeMEDfile"
+        print "CALCULATORPY::writeMEDfile : pointeur on first argument : ",field1
+        print "                          second         : ",fileName
+        print "CALCULATORPY::writeMEDfile : NOT YET IMPLEMENTED"
+        print "End of CALCULATORPY::writeMEDfile"
---
+++ src/PYCALCULATOR/PYCALCULATOR_TEST.py
@@ -0,0 +1,151 @@
+import salome
+import SALOME
+import SALOME_MED
+import PYCALCULATOR_ORB
+import os
+
+#==============================================================================
+
+def AnalyzeField(field):
+    name = field.getName()
+    desc = field.getDescription()
+    nbComp = field.getNumberOfComponents()
+    itNum = field.getIterationNumber()
+    ordNum = field.getOrderNumber()
+
+    print "Analysis of the field ",name," with the description ",desc
+    print "iteration number ",itNum," order Number ",ordNum
+    print "It has ",nbComp," component(s)"
+
+    fieldValue = field.getValue(SALOME_MED.MED_FULL_INTERLACE)
+    fieldSupport = field.getSupport()
+    fieldMesh = fieldSupport.getMesh()
+    fieldEntity = fieldSupport.getEntity()
+    bool = fieldSupport.isOnAllElements()
+
+    if bool:
+        print "The support of this field is on all entities ",fieldEntity," of the mesh ",fieldMesh.getName()
+        if fieldEntity == SALOME_MED.MED_NODE:
+            nbValByComp = fieldMesh.getNumberOfNodes()
+        else:
+            nbValByComp = fieldMesh.getNumberOfElements(fieldEntity,SALOME_MED.MED_ALL_ELEMENTS)
+        print "and its dimension (number of values by component of the field) is ",nbValByComp
+    else:
+        print "The support of this field is partially on entities ",fieldEntity," of the mesh ",fieldMesh.getName()
+        nbValByComp = fieldSupport.getNumberOfElements(SALOME_MED.MED_ALL_ELEMENTS)
+        print "and its dimension (number of values by component of the field) is ",nbValByComp
+
+    for i in range(nbComp):
+        compName = field.getComponentName(i+1)
+        compUnit = field.getComponentUnit(i+1)
+        print "The ",(i+1),"-th  component ",compName," with the unit ",compUnit
+
+    for i in range(nbValByComp):
+        print "  * ",fieldValue[i*nbComp:(i+1)*nbComp]
+
+#==============================================================================
+
+def getFieldIntObjectFromStudy(number,subnumber):
+    mySO = salome.myStudy.FindObject("MEDFIELD")
+    mysub = mySO.FindSubObject(number)[1]
+    if mysub:
+        mysubsub = mysub.FindSubObject(subnumber)[1]
+        if mysubsub:
+            Builder = salome.myStudy.NewBuilder()
+            anAttr = Builder.FindOrCreateAttribute(mysubsub, "AttributeIOR")
+            obj = salome.orb.string_to_object(anAttr.Value())
+            myObj = obj._narrow(SALOME_MED.FIELDINT)
+            return myObj
+        else:
+            print "ERROR: No FieldInt Object stored in this Study"
+            return None
+    else:
+        print "ERROR: No FieldInt Object stored in this Study"
+        return None
+
+#==============================================================================
+
+def getFieldDoubleObjectFromStudy(number,subnumber):
+    mySO = salome.myStudy.FindObject("MEDFIELD")
+    mysub = mySO.FindSubObject(number)[1]
+    if mysub:
+        mysubsub = mysub.FindSubObject(subnumber)[1]
+        if mysubsub:
+            Builder = salome.myStudy.NewBuilder()
+            anAttr = Builder.FindOrCreateAttribute(mysubsub, "AttributeIOR")
+            obj = salome.orb.string_to_object(anAttr.Value())
+            myObj = obj._narrow(SALOME_MED.FIELDDOUBLE)
+            return myObj
+        else:
+            print "ERROR: No FieldDouble Object stored in this Study"
+            return None
+    else:
+        print "ERROR: No FieldDouble Object stored in this Study"
+        return None
+
+#==============================================================================
+
+studyname = salome.myStudyName
+studynameId = salome.myStudyId
+
+print "We are working in the study ",studyname," with the ID ",studynameId
+print ""
+
+filePath=os.environ["MED_ROOT_DIR"]
+filePath=filePath+"/share/salome/resources/"
+medFile=filePath+"pointe.med"
+fieldname = "fieldcelldouble"
+
+print "Load the Med Component "
+med_comp = salome.lcc.FindOrLoadComponent("FactoryServer", "MED")
+
+try:
+    fieldcell  = med_comp.readFieldInFile(medFile,studyname,fieldname,-1,-1)
+except SALOME.SALOME_Exception, ex:
+    print ex.details
+    print ex.details.type
+    print ex.details.text
+    print ex.details.sourceFile
+    print ex.details.lineNumber
+    raise
+
+print fieldcell
+print fieldcell.getName()
+print fieldcell.getDescription()
+print fieldcell.getNumberOfComponents()
+
+
+AnalyzeField(fieldcell)
+
+
+
+print "Loading of the CalculatorPy Component "
+calculator = salome.lcc.FindOrLoadComponent("FactoryServerPy", "PYCALCULATOR")
+print "calculator = "
+dir(calculator)
+
+alpha = 2.0
+print "Multiplication of fieldcelldouble by alpha"
+fieldcellalpha = calculator.Mul(fieldcell,alpha)
+print fieldcellalpha
+AnalyzeField(fieldcellalpha)
+
+print "Adding of alpha*fieldcelldouble and fieldcelldouble"
+fieldcelladd = calculator.Add(fieldcell,fieldcellalpha)
+print fieldcelladd
+AnalyzeField(fieldcelladd)
+
+print "Put fieldcelladd in study"
+fieldcelladd.addInStudy(salome.myStudy,fieldcelladd)
+
+print "update Salome GUI"
+salome.sg.updateObjBrowser(1)
+
+print "Get back Field from study"
+# we grab the second field in MEDFIELD - first iteration
+fieldcelldouble = getFieldDoubleObjectFromStudy(2,1)
+print fieldcelldouble
+AnalyzeField(fieldcelldouble)
+
+
+print "Fin du test PYCALCULATOR"
