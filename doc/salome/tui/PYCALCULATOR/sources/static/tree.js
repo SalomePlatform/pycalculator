a2d7c47723bbdfe7305a42c0708de8787a0c209f
yfr <yfr@opencascade.com>
2004-06-22 15:45:30 +0000

    DCQ : Merge with Ecole Ete.

---
+++ doc/salome/tui/PYCALCULATOR/sources/static/tree.js
@@ -0,0 +1,18 @@
+foldersTree = gFld("<b>SALOME v.2.0.0 </b>", "", "")
+     insDoc(foldersTree, gLnk("Main Page", "", "main.html"))
+
+aux1 = insFld(foldersTree, gFld("TUI Reference Guide", ""))
+
+
+/*! Data structures
+*/
+         insDoc(aux1, gLnk("Data Structures", "", "annotated.html"))
+
+
+         insDoc(aux1, gLnk("Class Hierarchy", "", "hierarchy.html"))
+
+
+         insDoc(aux1, gLnk("Class methods list", "", "functions.html"))
+
+         insDoc(aux1, gLnk("File List", "", "files.html"))
+
