c14bac7ab81ccb678f912f9ad0c592c8df5ffc70
abd <abd@opencascade.com>
2008-11-27 14:03:51 +0000

    Update copyright information

--- src/PYCALCULATOR/PYCALCULATOR.py
+++ src/PYCALCULATOR/PYCALCULATOR.py
@@ -1,4 +1,7 @@
-#  Copyright (C) 2005  CEA/DEN, EDF R&D
+#  Copyright (C) 2007-2008  CEA/DEN, EDF R&D, OPEN CASCADE
+#
+#  Copyright (C) 2003-2007  OPEN CASCADE, EADS/CCR, LIP6, CEA/DEN,
+#  CEDRAT, EDF R&D, LEG, PRINCIPIA R&D, BUREAU VERITAS
 #
 #  This library is free software; you can redistribute it and/or
 #  modify it under the terms of the GNU Lesser General Public
@@ -14,13 +17,12 @@
 #  License along with this library; if not, write to the Free Software
 #  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
 #
-# See http://www.salome-platform.org/ or email : webmaster.salome@opencascade.com
+#  See http://www.salome-platform.org/ or email : webmaster.salome@opencascade.com
 #
-
-
 # Instruct Python to load dynamic libraries using global resolution of symbols
 # This is necessary to ensure that different modules will have the same definition
 # of dynamic types and C++ RTTI will work between them
+#
 import DLFCN, sys
 sys.setdlopenflags(DLFCN.RTLD_NOW | DLFCN.RTLD_GLOBAL)
 
